11cd41beb68df4c0150fd73b37b1be3dbc9529ff
Root Administrator <root@licboost.(none)>
2012-11-15 09:11:30 +0000

    more hacks! play on hours n stuff

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#chime").click(function() {
-        var socket = io.connect('http://localhost');
+        var socket = io.connect('http://192.168.1.135');
         socket.emit('chime');
     });
 });
--- server.js
+++ server.js
@@ -3,37 +3,80 @@ var express = require('express'),
     nodestatic = require('node-static'),
     async = require('async'),
     exec = require('child_process').exec;
+
+var adjhour = -1;
+
+var playHours = function() {
+    var ar = [];
+    for(var i = 8; i < (18+1); i++) {
+        ar.push(i);
+    }
+    return ar;
+}();
+console.log('Playing on hours: '+playHours);
+
+var Carillon = {
+    init: function() {
+        Carillon.hour = Carillon.fetchHour();
+        
+        async.whilst(function() {return true;},
+            function(callback) {
+                if(Carillon.hour!=Carillon.fetchHour()) {
+                    Carillon.hour = Carillon.fetchHour();
+                    Carillon.chime();
+                }
+                setTimeout(callback, 1000);
+            },
+            function (err) {}
+        );
+    },
     
-var Carillon = function() {
-    this.fetchHour = function() {
-        return Math.abs(12 - (new Date()).getHours());
-    };
-    this.hour = this.fetchHour();
+    _chime: function() {
+        if(playHours.indexOf(Carillon._fetchHour())!=-1) {
+            console.log("chime");
+
+            exec('play bells.wav', function (error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+            });
+        }
+    },
     
-    this._chime = function() {
-        exec('play bells.wav', function (error, stdout, stderr) {
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
-    };
-    
-    this.chime = function(x) {
+    chime: function(x) {
         if(typeof x == 'undefined') {
-            x = this.hour;
+            x = Carillon.hour;
         }
         
-        for(var i=0; i<x; i++) {
-            setTimeout(this._chime, 3000*(i+1));
+        console.log("chime"+x);
+        Carillon.x = x;
+        
+        exec('play doxology2.wav', function (error, stdout, stderr) {
+            for(var i = 0; i < Carillon.x; i++) {
+                setTimeout(Carillon._chime, 3000*(i+1));
+            }
+        });
+    },
+    
+    _fetchHour: function() {
+        return (new Date()).getHours() + adjhour;
+    },
+    
+    fetchHour: function() {
+        if(Carillon._fetchHour()==0) {
+            return 12;
+        } else {
+            return Math.abs((Carillon._fetchHour() < 13) ? Carillon._fetchHour() : (12 - Carillon._fetchHour()));
         }
-    };
+    }
 };
+
+console.log(Carillon._fetchHour() + ':' + Carillon.fetchHour());
 
 var app = express(),
     server = require('http').createServer(app),
     io = io.listen(server),
-    staticserver = new(nodestatic.Server)('./'),
-    carillon = new Carillon();
+    staticserver = new(nodestatic.Server)('./');
 
 //socket.io production settings
 io.enable('browser client minification');  // send minified client
@@ -49,30 +92,24 @@ io.set('transports', [                     // enable all transports (optional if
 ]);
 
 server.listen(5555);
+Carillon.init();
 
 io.sockets.on('connection', function (socket) {
     socket.on('chime', function (data) {
-        carillon.chime();
+        Carillon.chime();
     });
 });
 
 [ //static server
     '/',
     '/client.js',
+    '/style.css',
+    '/1140.css',
+    '/ie.css',
+    '/css3-mediaqueries.js',
     '/jquery-1.8.2.min.js'
 ].forEach(function(val, i, array) {
     app.get(val, function(req, res){
         staticserver.serve(req, res);
     });    
 });
-
-async.whilst(function() {return true;},
-    function(callback) {
-        if(carillon.hour!=carillon.fetchHour()) {
-            carillon.hour = carillon.fetchHour();
-            carillon.chime();
-        }
-        setTimeout(callback, 1000);
-    },
-    function (err) {}
-);
