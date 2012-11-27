8fcc472648a8d5245266dfad58d4a506dee8faae
David Volm <david@volminator.com>
2012-11-27 08:48:10 +0000

    Added thanksgiving music and other hacks.

--- server.js
+++ server.js
@@ -13,7 +13,78 @@ var playHours = function() {
     }
     return ar;
 }();
+
+Date.prototype.equals = function(date) { //check if a date(time) equals another date(time)
+    return this.getTime() == date.getTime();
+};
+
+Date.prototype.stripTime = function() { //remove the time from a date
+    return new Date(this.getFullYear(), this.getMonth(), this.getDate());
+};
+
+Date.prototype.plus = function(time, multiplier) { //date + time*multiplier
+    if(typeof multiplier == 'undefined') {
+        multiplier = 'ms';
+    }
+    
+    var mult = {
+        'ms': 1,
+        'sec': 1000,
+        'min': 1000*60,
+        'hour': 1000*60*60,
+        'day': 1000*60*60*24,
+        'week': 1000*60*60*24*7
+    };
+    
+    return new Date(this.getTime() + time * mult[multiplier]);
+};
+
+Date.prototype.doomsday = function() { //"Computer formula for the Doomsday of a year", simplified http://en.wikipedia.org/wiki/Doomsday_rule#Computer_formula_for_the_Doomsday_of_a_year
+    /* Day of week is an Integer 0-6
+     * Sunday , Monday, Tuesday, Wednesday , Thursday, Friday , and Saturday
+     * Noneday, Oneday, Twosday, Treblesday, Foursday, Fiveday, and Six-a-day
+     */
+
+    if(typeof this._doomsday_cache == 'undefined') { //if the doomsday is not cached
+        this._doomsday_cache = Math.round(((497*this.getFullYear()/400) + 2) % 7); //calculate and cache the doomsday
+    }
+    
+    return this._doomsday_cache; //return the cached doomsday
+};
+
+Date.prototype.thanksgiving = function() {
+    if(typeof this._thanksgiving_cache == 'undefined') { //if Thanksgiving day is not cached
+        var t = [ //Thanksgiving day in relation to doomsday
+            25, //Sunday
+            24, //Monday
+            23, //Tuesday
+            22, //Wednesday
+            28, //Thursday
+            27, //Friday
+            26, //Saturday
+        ];
+        this._thanksgiving_cache = new Date(this.getFullYear(), 10, t[this.doomsday()]); //calculate and cache Thanksgiving day
+    }
+    
+    return this._thanksgiving_cache; //return cached Thanksgiving day for this year 
+};
+
+Array.prototype.shuffle = function () {
+    for (var i = this.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = this[i];
+        this[i] = this[j];
+        this[j] = tmp;
+    }
+
+    return this;
+};
+
+var d = new Date();
 console.log('Playing on hours: '+playHours);
+console.log('Doomsday: ' + d.doomsday());
+console.log('Thanksgiving: ' + d.thanksgiving());
+console.log('Black Friday: ' + d.thanksgiving().plus(1, 'day'));
 
 var Carillon = {
     init: function() {
@@ -32,30 +103,45 @@ var Carillon = {
     },
     
     _chime: function() {
-        if(playHours.indexOf(Carillon._fetchHour())!=-1) {
-            console.log("chime");
+        console.log("chime");
 
-            exec('play bells.wav', function (error, stdout, stderr) {
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            });
+        exec('play bells.wav', function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+    },
+    
+    soundfile: function () {
+        var d = new Date().stripTime();
+        
+        //play thanksgiving music on Wednesday, Thursday, Friday, and Saturday
+        if(d.equals(d.thanksgiving()) || d.equals(d.thanksgiving().plus(-1, 'day')) || d.equals(d.thanksgiving().plus(1, 'day')) || d.equals(d.thanksgiving().plus(2, 'day'))) {
+            return [ //play some random Thanksgiving music
+                './lsb/782.wav',
+                './lsb/785.wav',
+                './lsb/892.wav',
+                './lsb/895.wav'
+            ].shuffle()[0];
+        } else {
+            'doxology.wav'; //not a holiday, play the doxology
         }
     },
     
     chime: function(x) {
-        if(typeof x == 'undefined') {
-            x = Carillon.hour;
-        }
-        
-        console.log("chime"+x);
-        Carillon.x = x;
-        
-        exec('play doxology2.wav', function (error, stdout, stderr) {
-            for(var i = 0; i < Carillon.x; i++) {
-                setTimeout(Carillon._chime, 3000*(i+1));
+        if(playHours.indexOf(Carillon._fetchHour())!=-1) {
+            if(typeof x == 'undefined') {
+                x = Carillon.hour;
             }
-        });
+            
+            console.log("chime"+x);
+            Carillon.x = x;
+            exec('play '+Carillon.soundfile(), function (error, stdout, stderr) {
+                for(var i = 0; i < Carillon.x; i++) {
+                    setTimeout(Carillon._chime, 3000*(i+1));
+                }
+            });
+        }
     },
     
     _fetchHour: function() {
@@ -63,15 +149,13 @@ var Carillon = {
     },
     
     fetchHour: function() {
-        if(Carillon._fetchHour()==0) {
+        if(Carillon._fetchHour()===0) {
             return 12;
         } else {
             return Math.abs((Carillon._fetchHour() < 13) ? Carillon._fetchHour() : (12 - Carillon._fetchHour()));
         }
     }
 };
-
-console.log(Carillon._fetchHour() + ':' + Carillon.fetchHour());
 
 var app = express(),
     server = require('http').createServer(app),
